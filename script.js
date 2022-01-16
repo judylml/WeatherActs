455339b3a08affd3dede2b879c84c52ab9edc7c8
raifahr <b2r8v@ugrad.cs.ubc.ca>
2022-01-15 21:23:41 -0800

    added custom city backgrounds

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
         document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
 
     searchWeather: function () {
